7aa174645c6f4cccd69e30141dbf31ac511bde68
Eyobalex <eyobalemayehu007@gmail.com>
2019-09-28 13:57:44 +0300

    Updates

---
+++ precache-manifest.2e0c5f1429bd78c0669abce3604b674b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957a8d8cf46216679ba7cffcaec86046",
+    "url": "/hrs-react/index.html"
+  },
+  {
+    "revision": "23d851a3c4ccbebcb26f",
+    "url": "/hrs-react/static/css/2.c3e87adf.chunk.css"
+  },
+  {
+    "revision": "0fffdc3476f292012ce8",
+    "url": "/hrs-react/static/css/main.8fd93e17.chunk.css"
+  },
+  {
+    "revision": "23d851a3c4ccbebcb26f",
+    "url": "/hrs-react/static/js/2.2fff60ff.chunk.js"
+  },
+  {
+    "revision": "0fffdc3476f292012ce8",
+    "url": "/hrs-react/static/js/main.2575abb6.chunk.js"
+  },
+  {
+    "revision": "592480ec6cdbd7c2480c",
+    "url": "/hrs-react/static/js/runtime-main.9490dfb5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/hrs-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/hrs-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/hrs-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/hrs-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/hrs-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
